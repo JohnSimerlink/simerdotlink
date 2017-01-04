6b19a71d0291ac4a5fdf257310ea58a2c3b80233
JohnSimerlink <johnsimerlink@gmail.com>
2017-01-04 00:13:18 +0000

    resume

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-//document.getElements
+//[].forEach.call(document.getElementsByClassName('project-name'), function(e) {
+//	elem = e;
+//	console.log("element ", e, " found");
+//	e.addEventListener('click', function (ev){
+//		ev.preventDefault();
+//		ev.stopPropagation();
+//		console.log('click called');
+//		 description = e.parentNode.getElementsByClassName('description')[0];
+//		var className = description.className;
+//		console.log('class name is', className);
+//		var hidden = className.search('hidden') > -1;
+//		className = hidden ? className.replace(' hidden', '') : (className + ' hidden');
+//		console.log('class name is now', className);
+//		description.className = className;
+//	}, false);
+//})
